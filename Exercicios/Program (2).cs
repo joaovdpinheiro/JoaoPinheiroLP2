2f67767276bc8a6200450f5e65f67efe2506e830
joao pinheiro <joaovdpinheiro21@gmail.com>
2015-04-07 15:48:04 -0300

    trabalhos

---
+++ Exercicios/Program (2).cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Collections;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio_com_array_joao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList numeros = new ArrayList();
+            int numero = 1;
+            int soma = 0;
+            int media;
+
+            for (int i = 0; numero != 0; i++)
+            {
+                Console.WriteLine("Digite um valor de um número: ");
+                numero = int.Parse(Console.ReadLine());
+
+                numeros.Add(numero);
+                numeros.Remove(0);
+
+                soma += numero;
+            }
+            Console.WriteLine();
+
+            media = soma / numeros.Count;
+            Console.WriteLine("A média é: " + media);
+
+        }
+    }
+}
---
+++ Exercicios/sem_array_joao.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sem_array_joao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 0;
+            int media;
+            int n = 1;
+            int i = 0;
+
+            while (n != 0)
+            {
+                Console.WriteLine("Digite um valor para n: ");
+                n = int.Parse(Console.ReadLine());
+
+                num += n;
+                i++;
+            }
+
+            Console.WriteLine();
+            media = num / (i - 1);
+
+            Console.WriteLine("A média aritmética dos números é: " + media);
+        }
+    }
+}
