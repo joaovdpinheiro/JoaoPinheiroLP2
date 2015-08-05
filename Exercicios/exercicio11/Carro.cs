631cf3e1cb0d1248a4cdceec1a2f262f26b79e8a
joaovdpinheiro <joaovdpinheiro21@gmail.com>
2015-08-04 23:53:23 -0300

    Exercicios segunda cert

---
+++ Exercicios/exercicio11/Carro.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio11
+{
+    class Carro
+    {
+        private string _modelo;
+        private string _marca;
+        private string _placa;
+        private int _id = 0;
+        private int _numeroPassageiros;
+
+        public void SetModelo(string modelo)
+        {
+            this._modelo = modelo;
+        }
+
+        public string GetModelo()
+        {
+            return _modelo;
+        }
+
+        public void SetMarca(string marca)
+        {
+            this._marca = marca;
+        }
+
+        public string GetMarca()
+        {
+            return _marca;
+        }
+
+        public void SetPlaca(string placa)
+        {
+            this._placa = placa;
+        }
+
+        public string GetPlaca()
+        {
+            return _placa;
+        }
+
+        public void SetNumeroPassageiros(int numPassageiros)
+        {
+            this._numeroPassageiros = numPassageiros;
+        }
+
+        public int GetNumeroPassageiros()
+        {
+            return _numeroPassageiros;
+        }
+
+        public void SetValueToId()
+        {
+            _id = _id + 1;
+        }
+
+        public int GetId()
+        {
+            return _id;
+        }
+    }
+}
---
+++ Exercicios/exercicio11/Program.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe a quantidade de carros: ");
+            int numeroCarros = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            int totalPessoas = 0;
+
+            List<Carro> TotalCarros = new List<Carro>();
+
+
+            for (int i = 0; i < numeroCarros; i++)
+            {
+                Carro carros = new Carro();
+
+                Console.WriteLine("Digite a placa do carro: ");
+                string placa = Console.ReadLine();
+                Console.WriteLine();
+
+                Console.WriteLine("Digite o modelo do carro: ");
+                string modelo = Console.ReadLine();
+                Console.WriteLine();
+
+                Console.WriteLine("Digite a marca do carro: ");
+                string marca = Console.ReadLine();
+                Console.WriteLine();
+
+                Console.WriteLine("Digite a capacidade de passageiros no carro: ");
+                int numPassageiros = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine();
+
+                carros.SetPlaca(placa);
+                carros.SetModelo(modelo);
+                carros.SetMarca(marca);
+                carros.SetNumeroPassageiros(numPassageiros);
+                carros.SetValueToId();
+
+                carros.GetId();
+                carros.GetPlaca();
+                carros.GetModelo();
+                carros.GetMarca();
+
+                TotalCarros.Add(carros);
+            }
+
+            int numeroCarrosLotados = 0;
+
+            Console.WriteLine("Digite o ID do carro em que deseja viajar: ");
+            int j = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+
+            int L = j;
+
+
+            while (j != -1)
+            {
+                if (L != j)
+                {
+                    Console.WriteLine("Digite o ID do carro em que deseja viajar: ");
+                    j = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine();
+
+                    if (j == -1)
+                    {
+                        break;
+                    }
+
+                }
+                L = -1;
+
+                if (j > TotalCarros.Count)
+                {
+                    Console.WriteLine("Carro invalido");
+                    Console.WriteLine();
+                }
+
+                if (j <= TotalCarros.Count)
+                {
+                    if (TotalCarros[j - 1].GetNumeroPassageiros() > 0)
+                    {
+                        int vaga = TotalCarros[j - 1].GetNumeroPassageiros();
+                        vaga--;
+                        TotalCarros[j - 1].SetNumeroPassageiros(vaga);
+                        totalPessoas++;
+
+                        if (vaga == 0)
+                        {
+                            numeroCarrosLotados++;
+                        }
+                    }
+
+                    else
+                    {
+                        Console.WriteLine("O carro solicitado já está lotado!");
+                        Console.WriteLine();
+
+                        if (numeroCarrosLotados == numeroCarros)
+                        {
+                            break;
+                        }
+
+                    }
+                }
+
+
+            }
+
+            for (int k = 0; k < numeroCarros; k++)
+            {
+                Console.WriteLine("Carro: " + " Id- " + (k + 1) + " Marca- " + TotalCarros[k].GetMarca() + " Modelo- " + TotalCarros[k].GetModelo() + " Placa- " + TotalCarros[k].GetPlaca());
+                Console.WriteLine("Vagas Restantes: " + TotalCarros[k].GetNumeroPassageiros());
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("Número de pessoas na viagem: " + totalPessoas);
+
+            Console.ReadKey();
+        }
+    }
+}
