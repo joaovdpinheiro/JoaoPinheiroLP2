f4b608f422d704e182639c30189905cfefde9048
joaovdpinheiro <joaovdpinheiro21@gmail.com>
2015-10-20 15:52:19 -0300

    Exercicios

---
+++ Exercicios/3C_Exercicio12/3C_Exercicio12/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3C_Exercicio12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Funcionario> funcionario = new List<Funcionario>;
+            Console.WriteLine("Qtd Funcionarios:");
+            int qtdFuncs = int.Parse(Console.ReadLine());
+                       
+           
+            for (int i = 1; i <= qtdFuncs; i++)
+            {
+                Funcionario f = new Funcionario();
+                
+                Console.WriteLine("Nome :");
+                f.Set_nome(Console.ReadLine());
+
+                Console.WriteLine("Matricula :");
+                f.Set_matricula(Console.ReadLine());
+
+                Console.WriteLine("CPF :");
+                f.Set_cpf(Console.ReadLine());
+
+                Console.WriteLine("Data Nascimento :");
+                f.Set_dataNascimento(Console.ReadLine());
+                funcionario.Add(f);  
+               
+            }//fim for - leitura 
+
+
+
+            for (int k = 0; k < funcionario.Count; k++)
+            {
+                Funcionario func = new Funcionario();
+
+                func = (Funcionario)funcionario[k];
+
+                
+             
+           
+            }// for carros.Count
+
+         
+        
+        }
+    }
+}
---
+++ Exercicios/3C_Exercicio12/3C_Exercicio12/Trabalhador.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3C_Exercicio12
+{
+    class Trabalhador: Funcionario
+    {
+        private double _salario;
+
+        public void Set_salario(double salario)
+        {
+            _salario = salario;
+        }
+
+        public double Get_salario()
+        {
+            return _salario;
+        }
+
+
+        public double ValorSalario()
+        {
+            return _salario; 
+        }
+
+        
+        public double ValeAlimentacao()
+        {
+            double valor;
+            double valor_maximo = 750;
+
+            valor = _salario * 8 / 100;
+
+            if (valor >= valor_maximo)
+            {
+                return valor_maximo;
+            }
+            else
+            {
+                return valor;
+            }
+
+        }   
+   
+    }
+}
